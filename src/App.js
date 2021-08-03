36e794dbd08e189bd7f90f14e70274da2e8bc612
unknown <ssgsj.carranza@gmail.com>
2021-08-02 21:48:13 -0500

    intalled react router and set up some path routing, the idea being after a user takes a pic, they are redirected to a preview screen, kind of like what snapchat would do

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>SNAPCHAT</h1>
-      <WebcamCapture />
+      <Router>
+      <div className="app__bodu">
+       <Switch>
+          <Route exact path="/">
+          <WebcamCapture />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
