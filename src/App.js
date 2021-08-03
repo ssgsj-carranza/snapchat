2c1694d2da8b0b64c7b5a9054e5435ea36f06b1e
unknown <ssgsj.carranza@gmail.com>
2021-08-02 21:54:21 -0500

    set up redirect on capture to preview page using useHsitory com react router dom, to push the redirect

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Preview from './Preview';
 
 function App() {
   return (
@@ -9,8 +10,11 @@ function App() {
       <Router>
       <div className="app__bodu">
        <Switch>
+          <Route path="/preview">
+            <Preview />
+          </Route>
           <Route exact path="/">
-          <WebcamCapture />
+            <WebcamCapture />
           </Route>
         </Switch>
       </div>
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -3,6 +3,7 @@ import Webcam from 'react-webcam';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import {useDispatch} from 'react-redux';
 import { setCameraImage } from './features/cameraSlice';
+import { useHistory } from 'react-router-dom';
 
 const videoConstraints = {
     width: 250,
@@ -13,10 +14,12 @@ const videoConstraints = {
 function WebcamCapture() {
     const webcamRef = useRef(null);
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const capture = useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
         dispatch(setCameraImage(imageSrc));
+        history.push('/preview')
     },[webcamRef])
 
     return (
