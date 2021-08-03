d759800fb827bd26e20531300b5bae83d3913442
unknown <ssgsj.carranza@gmail.com>
2021-08-02 22:24:26 -0500

    set up use effect to redirect user to homepage if there isnt an image captured

--- src/Preview.js
+++ src/Preview.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Preview.css';
 import {useSelector} from 'react-redux';
 import {selectCameraImage} from './features/cameraSlice';
+import {useHistory} from 'react-router-dom';
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage);
+    const history = useHistory();
+
+    useEffect (() => {
+        if (!cameraImage) {
+            history.replace('/')
+        }
+    },[cameraImage, history]);
 
     return (
         <div className="preview">
