69c2c74ab0d1fd334126a3fd36ad9b149660eeca
unknown <ssgsj.carranza@gmail.com>
2021-08-02 22:08:19 -0500

    using redux to pull camera image from data layer to be displayed in preview page

--- src/Preview.js
+++ src/Preview.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Preview.css';
+import {useSelector} from 'react-redux';
 
 function Preview() {
+    // const cameraImage = useSelector
+
     return (
         <div className="preview">
-            
+            <img src='' alt=''/>        
         </div>
     )
 }
