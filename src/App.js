14623a57391d74bcb698af322fa4a71d1b35a4a1
unknown <ssgsj.carranza@gmail.com>
2021-08-03 21:32:37 -0500

    began work on the header section of the chats page and styled it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import WebcamCapture from './WebcamCapture';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Preview from './Preview';
+import Chats from './Chats';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Router>
       <div className="app__body">
        <Switch>
+       <Route path="/chats">
+            <Chats />
+          </Route>
           <Route path="/preview">
             <Preview />
           </Route>
