0911f90bc5738efc4eaaaecbd659ed275243290c
unknown <ssgsj.carranza@gmail.com>
2021-08-03 20:34:37 -0500

    added close icon on preview page to reset camera image back to null with redux action

--- src/Preview.js
+++ src/Preview.js
@@ -1,12 +1,14 @@
 import React, {useEffect} from 'react'
 import './Preview.css';
-import {useSelector} from 'react-redux';
-import {selectCameraImage} from './features/cameraSlice';
+import {useSelector, useDispatch} from 'react-redux';
+import {resetCameraImage, selectCameraImage} from './features/cameraSlice';
 import {useHistory} from 'react-router-dom';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage);
     const history = useHistory();
+    const dispatch = useDispatch();
 
     useEffect (() => {
         if (!cameraImage) {
@@ -14,9 +16,13 @@ function Preview() {
         }
     },[cameraImage, history]);
 
+    const closePreview = () => {
+        dispatch(resetCameraImage());
+    }
+
     return (
         <div className="preview">
-            <h2>preview</h2>
+            <CloseIcon onClick={closePreview} className="preview__close"/>
             <img src={cameraImage} alt=''/>        
         </div>
     )
