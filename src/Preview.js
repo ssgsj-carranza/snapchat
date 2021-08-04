4f1a0c7f11bab6e05ce68263444f25304af15105
unknown <ssgsj.carranza@gmail.com>
2021-08-03 20:46:17 -0500

    pulled in more icons to add to the tool bar and styled in to mimic snapchat

--- src/Preview.js
+++ src/Preview.js
@@ -4,6 +4,13 @@ import {useSelector, useDispatch} from 'react-redux';
 import {resetCameraImage, selectCameraImage} from './features/cameraSlice';
 import {useHistory} from 'react-router-dom';
 import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage);
@@ -23,6 +30,15 @@ function Preview() {
     return (
         <div className="preview">
             <CloseIcon onClick={closePreview} className="preview__close"/>
+            <div className="preview__toolbarRight">
+                <TextFieldsIcon />
+                <CreateIcon />
+                <NoteIcon />
+                <MusicNoteIcon />
+                <AttachFileIcon />
+                <CropIcon />
+                <TimerIcon />
+            </div>
             <img src={cameraImage} alt=''/>        
         </div>
     )
