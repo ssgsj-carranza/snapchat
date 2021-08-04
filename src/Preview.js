19ee2c0230c14f0eefd24e760ab5fc208467544b
unknown <ssgsj.carranza@gmail.com>
2021-08-03 21:16:28 -0500

    created send post function to generate a unique id and upload the image using that id, and to redirect to a chat screen on complition, working on chat page next

--- src/Preview.js
+++ src/Preview.js
@@ -12,6 +12,9 @@ import AttachFileIcon from '@material-ui/icons/AttachFile';
 import CropIcon from '@material-ui/icons/Crop';
 import TimerIcon from '@material-ui/icons/Timer';
 import SendIcon from '@material-ui/icons/Send';
+import {v4 as uuid} from 'uuid';
+import {storage, db} from './firebase';
+import firebase from 'firebase';
 
 function Preview() {
     const cameraImage = useSelector(selectCameraImage);
@@ -26,7 +29,30 @@ function Preview() {
 
     const closePreview = () => {
         dispatch(resetCameraImage());
-    }
+    };
+
+    const sendPost = () => {
+        // GENERATES UNIQUE STRING TO AVOID COLLISIONS
+        const id = uuid();
+        //UPLOAD IMAGE
+        const uploadTask = storage.ref(`posts/${id}`).putString(cameraImage, 'data_url');
+        uploadTask.on('state_changed', null, (error) => {
+            //ERROR FUNCTION
+            console.log(error)
+        }, () => {
+            // COMPLETE FUNCTION
+            storage.ref('posts').child(id).getDownloadURL().then((url) => {
+                db.collection('posts').add({
+                    image: url,
+                    username: 'Jorge',
+                    read: false,
+                    //profilePic
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                });
+                history.replace('/chats');
+            });
+        });
+    };
 
     return (
         <div className="preview">
@@ -41,7 +67,7 @@ function Preview() {
                 <TimerIcon />
             </div>
             <img src={cameraImage} alt=''/>
-            <div className="preview__footer">
+            <div onClick={sendPost} className="preview__footer">
                 <h2>Send</h2>
                 <SendIcon fontSize='small' className="preview__sendIcon"/>
             </div>        
