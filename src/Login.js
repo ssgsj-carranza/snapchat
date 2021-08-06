e1bf97bc66c01ac62ae1e5b136aa9bee330fb578
unknown <ssgsj.carranza@gmail.com>
2021-08-05 20:07:18 -0500

    styled signin page, workin on signin functionality

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,18 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import './Login.css'
 
 function Login() {
+    const singIn = () => {
+
+    };
+
     return (
-        <div>
-            
+        <div className="login">
+            <div className="login__container">
+                <img src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg" alt="" />
+                <Button variant="outlined" onClick={singIn}>Sign In</Button>
+            </div>        
         </div>
     )
 }
